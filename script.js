31324cbbeab9d9a4a0f92da2821794dc533c0607
longnguyen1803 <longnguyen180392@gmail.com>
2016-03-15 15:00:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,21 +13,7 @@ var photos = [ {
 		"url" : "http://www.sxc.hu/photo/1271909",
 		"firstline" : "Going on",
 		"secondline" : "vacation?"
-	}, {
-		"title" : "Office Appartments",
-		"image" : "work.jpg",
-		"url" : "http://www.sxc.hu/photo/1265695",
-		"firstline" : "Or still busy at",
-		"secondline" : "work?"
-	}, {
-		"title" : "Mountainbiking",
-		"image" : "biking.jpg",
-		"url" : "http://www.sxc.hu/photo/1221065",
-		"firstline" : "Get out and be",
-		"secondline" : "active"
-	}, {
-		"title" : "Mountains Landscape",
-		"image" : "nature.jpg",
+	
 		"url" : "http://www.sxc.hu/photo/1271915",
 		"firstline" : "Take a fresh breath of",
 		"secondline" : "nature"
